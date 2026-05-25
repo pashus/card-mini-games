c74939843be9c2bbad4956aedfae205fd66bb3ad
Pavel Martynov <25pashok@mail.ru>
2026-05-25 21:22:16 +0500

    Revert "revert-me-test"
    This reverts commit 25d81735ccff2f54f2e55a6e08c03a24da968cc9.

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="bg-header flex min-h-20 flex-col items-center gap-1 px-12 py-2 text-center sm:flex-row sm:justify-between sm:gap-0">
       <Link to="/" className="cursor-pointer text-3xl font-bold select-none">
-        КАРТОЧНЫЕ ИГРУШКИ жопа
+        КАРТОЧНЫЕ ИГРУШКИ
       </Link>
       <div className="flex gap-4">
         <Link className="text-2xl" to="#">
